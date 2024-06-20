8b1180c98bc848745f621370d60c2bcb4ed0a22e
Stanislav Baiduzhyi <stanislav.baiduzhyi@kape.com>
2024-06-20 09:34:16 +0100

    CVPN-956: renaming metric for consistency

--- lightway-app-utils/src/metrics.rs
+++ lightway-app-utils/src/metrics.rs
@@ -1,8 +1,8 @@
 use metrics::counter;
 
-const METRIC_TUN_IOURING_DATA_DROPPED: &str = "tun_iouring_data_dropped";
+const METRIC_TUN_IOURING_PACKET_DROPPED: &str = "tun_iouring_packet_dropped";
 
 /// Counter for "sending into a full channel" type of error ([`async_channel::TrySendError::Full`])
-pub(crate) fn tun_iouring_data_dropped() {
-    counter!(METRIC_TUN_IOURING_DATA_DROPPED).increment(1);
+pub(crate) fn tun_iouring_packet_dropped() {
+    counter!(METRIC_TUN_IOURING_PACKET_DROPPED).increment(1);
 }
