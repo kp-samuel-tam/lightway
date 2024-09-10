d1bdea68fe6a7a384cd8b64b412da89ba7b3baac
Ian Campbell <52475242+xv-ian-c@users.noreply.github.com>
2024-09-10 16:20:50 +0100

    lightway-app-utils: iouring: Rework to push more processing into uring thread
    This removes the need for the send and receive tasks posting sqe's to the
    thread for it to post by doing that as part of the loop. It also pulls the cqe
    processing into the same loop.
    
    This now uses a model similar to that in most of the uring examples (in both C
    and Rust).
    
    This change improves an iperf upload test from 2.03 Gbits/sec to 2.19 Gbigs/sec
    Gbits/sec and download from 2.34 Gbits/sec to 2.46 Gbits/sec.
    
    The channel used for the tx and rx queues is changed from the async_channel to
    tokio::mpsc. The performance difference appears to be in the noise, however we
    no longer need the multi-consumer property (in fact we'd be happy with a spsc
    channel if there was one) and we use tokio::mpsc everywhere else.

--- lightway-app-utils/src/metrics.rs
+++ lightway-app-utils/src/metrics.rs
@@ -1,16 +1,70 @@
-use metrics::{counter, histogram, Counter, Histogram};
-use std::sync::LazyLock;
+use metrics::{counter, gauge, histogram, Counter, Gauge, Histogram};
+use std::{sync::LazyLock, time::Duration};
 
 static METRIC_TUN_IOURING_PACKET_DROPPED: LazyLock<Counter> =
     LazyLock::new(|| counter!("tun_iouring_packet_dropped"));
 static METRIC_TUN_IOURING_COMPLETION_BATCH_SIZE: LazyLock<Histogram> =
     LazyLock::new(|| histogram!("tun_iouring_completion_batch_size"));
+static METRIC_TUN_IOURING_COMPLETIONS_BEFORE_BLOCKING: LazyLock<Histogram> =
+    LazyLock::new(|| histogram!("tun_iouring_completions_before_blocking"));
 
-/// Counter for "sending into a full channel" type of error ([`async_channel::TrySendError::Full`])
+static METRIC_TUN_IOURING_RX_EAGAIN: LazyLock<Counter> =
+    LazyLock::new(|| counter!("tun_iouring_rx_eagain"));
+static METRIC_TUN_IOURING_RX_ERR: LazyLock<Counter> =
+    LazyLock::new(|| counter!("tun_iouring_rx_err"));
+
+static METRIC_TUN_IOURING_BLOCKED: LazyLock<Counter> =
+    LazyLock::new(|| counter!("tun_iouring_blocked"));
+static METRIC_TUN_IOURING_WAKE_EVENTFD: LazyLock<Counter> =
+    LazyLock::new(|| counter!("tun_iouring_wake_eventfd"));
+static METRIC_TUN_IOURING_WAKE_TX: LazyLock<Counter> =
+    LazyLock::new(|| counter!("tun_iouring_wake_tx"));
+
+static METRIC_TUN_IOURING_TOTAL_THREAD_TIME: LazyLock<Gauge> =
+    LazyLock::new(|| gauge!("tun_iouring_total_thread_time"));
+static METRIC_TUN_IOURING_IDLE_THREAD_TIME: LazyLock<Gauge> =
+    LazyLock::new(|| gauge!("tun_iouring_idle_thread_time"));
+
+/// Counter for "sending into a full channel" type of error ([`tokio::sync::mpsc::error::TrySendError::Full`])
 pub(crate) fn tun_iouring_packet_dropped() {
     METRIC_TUN_IOURING_PACKET_DROPPED.increment(1);
 }
 
+/// Monitors size of uring completion queue on each iteration
 pub(crate) fn tun_iouring_completion_batch_size(sz: usize) {
     METRIC_TUN_IOURING_COMPLETION_BATCH_SIZE.record(sz as f64);
+}
+
+pub(crate) fn tun_iouring_completions_before_blocking(sz: usize) {
+    METRIC_TUN_IOURING_COMPLETIONS_BEFORE_BLOCKING.record(sz as f64)
+}
+
+/// Count iouring RX entries which complete with EAGAIN
+pub(crate) fn tun_iouring_rx_eagain() {
+    METRIC_TUN_IOURING_RX_EAGAIN.increment(1)
+}
+
+/// Count iouring RX entries which complete with an error
+pub(crate) fn tun_iouring_rx_err() {
+    METRIC_TUN_IOURING_RX_ERR.increment(1)
+}
+
+pub(crate) fn tun_iouring_blocked() {
+    METRIC_TUN_IOURING_BLOCKED.increment(1)
+}
+
+pub(crate) fn tun_iouring_wake_eventfd() {
+    METRIC_TUN_IOURING_WAKE_EVENTFD.increment(1)
+}
+
+pub(crate) fn tun_iouring_wake_tx() {
+    METRIC_TUN_IOURING_WAKE_TX.increment(1)
+}
+
+pub(crate) fn tun_iouring_total_thread_time(t: Duration) {
+    METRIC_TUN_IOURING_TOTAL_THREAD_TIME.set(t.as_millis() as f64);
+}
+
+pub(crate) fn tun_iouring_idle_thread_time(t: Duration) {
+    METRIC_TUN_IOURING_IDLE_THREAD_TIME.increment(t.as_millis() as f64);
 }
