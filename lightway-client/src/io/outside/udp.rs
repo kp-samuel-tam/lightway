c34eca25cb1db887db308800af2ff41d28847b81
Max <max.li@kape.com>
2024-10-02 10:07:43 +0800

    Ignore network unreachable socket error for linux-like OS when sending out packets from the udp socket of lightway-client.
    If this error is not ignored, it will be passed to the WolfSSL layer. Then, the WolfSSL layer will reach a fatal socket error state and stop the inside IO loop during network change.
    Ignoring this error to prevent the IO loop from exiting.
    This change only applies to linux-like OS. Other OS like Windows may need a different implementation.

--- lightway-client/src/io/outside/udp.rs
+++ lightway-client/src/io/outside/udp.rs
@@ -82,6 +82,22 @@ impl OutsideIOSendCallback for Udp {
                 // This way we can continue if/when the server shows up.
                 IOCallbackResult::Ok(0)
             }
+            Err(err) if matches!(err.raw_os_error(), Some(libc::ENETUNREACH)) => {
+                // This case indicates network unreachable error.
+                // Possibly there is a network change at the moment.
+                //
+                // Swallow the socket error so the error is not passed to the
+                // WolfSSL layer. Then the WolfSSL layer would not enter a
+                // fatal error state
+                //
+                // Note: this case should be matched by
+                // matches!(err.kind(), std::io::ErrorKind::NetworkUnreachable)
+                // However, at the time of implementing this match case,
+                // the version of rust we use does not support NetworkUnreachable.
+                // This match case can be rewritten to use NetworkUnreachable
+                // once rust is updated to support it.
+                IOCallbackResult::Ok(0)
+            }
             Err(err) => IOCallbackResult::Err(err),
         }
     }
