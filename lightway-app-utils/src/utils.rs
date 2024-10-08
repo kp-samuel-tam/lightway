411d8460889ee6364135566dd029c7c8f490b893
Ian Campbell <52475242+xv-ian-c@users.noreply.github.com>
2024-10-08 09:05:56 +0100

    Relax permissions checks on configuration files
    In 23c30e61c7e6 ("lightway-app-utils: Use `fs_mistrust` to validate config file
    safety.") restrictions were added to ensure that configuration files and
    secrets had permissions which would prevent another user from tampering with
    them. This also required that the files were not readable by other users.
    
    As of b8e2fc51e0ef ("lightway-server: Support multiple users") the
    configuration file no longer contains any sensitive secrets so we can be more
    tolerant of the file being readable.
    
    We continue to enforce non-readableness for the server key and the user
    database. We also add the missing checks for the server certificate, which we
    allow to be readable.

--- lightway-app-utils/src/utils.rs
+++ lightway-app-utils/src/utils.rs
@@ -3,15 +3,30 @@ use std::path::PathBuf;
 use anyhow::Result;
 use fs_mistrust::Mistrust;
 
+/// Specifies the limits which `validate_configuration_file_path`
+/// should enforce.
+pub enum Validate {
+    /// Ensure that only the file's owner can access (read or write)
+    OwnerOnly,
+    /// Ensure that only the file's owner can write but allow anyone
+    /// to read
+    AllowWorldRead,
+}
+
 /// Verifies whether input `path` points to a valid configuration file
 /// present in filesystem. Validates that the ownership permissions
 /// are acceptable.
-pub fn validate_configuration_file_path(path: &PathBuf) -> Result<()> {
-    Mistrust::builder()
+pub fn validate_configuration_file_path(path: &PathBuf, validate: Validate) -> Result<()> {
+    let mistrust = Mistrust::builder()
         .controlled_by_env_var("LW_DANGEROUSLY_DISABLE_PERMISSIONS_CHECKS")
-        .build()?
-        .verifier()
-        .require_file()
-        .check(path)?;
+        .build()?;
+
+    let verifier = mistrust.verifier().require_file();
+    let verifier = match validate {
+        Validate::OwnerOnly => verifier,
+        Validate::AllowWorldRead => verifier.permit_readable(),
+    };
+
+    verifier.check(path)?;
     Ok(())
 }
