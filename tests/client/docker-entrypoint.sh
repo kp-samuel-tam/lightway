ef8ab6d765e8a00bdfa68170e0be309ae37074d8
kp-brandon-chew <brandon.chew@kape.com>
2025-07-24 09:03:13 +0800

    client: Fix user being unable to state server hostname
    Previously parsing a non-ip hostname to the config
    would throw an error
    This fixes it so that the hostname is resolve
    in the binary

--- tests/client/docker-entrypoint.sh
+++ tests/client/docker-entrypoint.sh
@@ -10,20 +10,9 @@ echo "Ping server"
 echo "====================================================================="
 ping -W 1 -c 3 server
 
-server=$(dig +short server)
-
-# Add server argument using resolved IP and SERVER_PORT env var
-if [ -n "${SERVER_PORT:-}" ]; then
-    server_arg="--server=$server:$SERVER_PORT"
-    echo "Using server address: $server:$SERVER_PORT"
-else
-    server_arg=""
-    echo "No SERVER_PORT set, using arguments from command line"
-fi
-
 echo ""
 echo "====================================================================="
-echo "Start lightway-client: $* $server_arg"
+echo "Start lightway-client: $*"
 echo "====================================================================="
 
-exec ./lightway-client "$@" "$server_arg"
+exec ./lightway-client "$@"
