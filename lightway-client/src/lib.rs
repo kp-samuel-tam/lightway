d4e676b72465cca3e4d71bb7ebf8d56ba1a3641f
Wei Wen Goh <weiwen.goh@kape.com>
2025-08-04 10:16:25 +0800

    client: set InsideIO only after best connection is selected
    Previously, this was set when the connection goes online. Now that we
    have multiple simultaneous connections, we set it only for the best
    connection.

--- lightway-client/src/lib.rs
+++ lightway-client/src/lib.rs
@@ -17,9 +17,8 @@ use lightway_app_utils::{
 };
 use lightway_core::{
     BuilderPredicates, ClientContextBuilder, ClientIpConfig, Connection, ConnectionError,
-    ConnectionType, Event, EventCallback, IOCallbackResult, InsideIOSendCallback,
-    InsideIOSendCallbackArg, InsideIpConfig, OutsidePacket, State, ipv4_update_destination,
-    ipv4_update_source,
+    ConnectionType, Event, EventCallback, IOCallbackResult, InsideIOSendCallbackArg,
+    InsideIpConfig, OutsidePacket, State, ipv4_update_destination, ipv4_update_source,
 };
 use tokio::sync::mpsc::UnboundedReceiver;
 
@@ -92,7 +91,7 @@ pub struct ClientConfig<'cert, ExtAppState: Send + Sync> {
     /// Alternate Inside IO to use
     /// When this is supplied, tun_config will not be used for creating tun interface
     #[educe(Debug(ignore))]
-    pub inside_io: Option<Arc<dyn InsideIO<T>>>,
+    pub inside_io: Option<Arc<dyn InsideIO<ExtAppState>>>,
 
     /// Tun device to use
     pub tun_config: TunConfig,
@@ -271,7 +270,6 @@ async fn handle_events<A: 'static + Send + EventCallback, ExtAppState: Send + Sy
     weak: Weak<Mutex<Connection<ConnectionState<ExtAppState>>>>,
     enable_encoding_when_online: bool,
     event_handler: Option<A>,
-    inside_io: Arc<dyn InsideIOSendCallback<ConnectionState<ExtAppState>> + Send + Sync>,
     connected_signal: oneshot::Sender<()>,
 ) {
     let mut connected_signal = Some(connected_signal);
@@ -286,8 +284,6 @@ async fn handle_events<A: 'static + Send + EventCallback, ExtAppState: Send + Sy
                     let Some(conn) = weak.upgrade() else {
                         break; // Connection disconnected.
                     };
-
-                    conn.lock().unwrap().inside_io(inside_io.clone());
 
                     if enable_encoding_when_online
                         && let Err(e) = conn.lock().unwrap().set_encoding(true)
@@ -517,8 +513,9 @@ pub async fn encoding_request_task<ExtAppState: Send + Sync>(
 }
 
 /// Represents a connection to a server. When dropped, the route table will be removed.
-pub struct ClientConnection<T> {
+pub struct ClientConnection<T: Send + Sync> {
     task: JoinHandle<anyhow::Result<ClientResult>>,
+    conn: Arc<Mutex<Connection<ConnectionState<T>>>>,
     inside_io: Arc<dyn io::inside::InsideIO<T>>,
     outside_io: Arc<dyn io::outside::OutsideIO>,
     connected_signal: Option<oneshot::Receiver<()>>,
@@ -555,6 +552,12 @@ impl<ExtAppState: Send + Sync> ClientConnection<ExtAppState> {
 
         self.route_table = Some(route_table);
         Ok(())
+    }
+
+    pub fn set_connection_inside_io(&self) {
+        let inside_io: InsideIOSendCallbackArg<ConnectionState<ExtAppState>> =
+            self.inside_io.clone().into_io_send_callback();
+        self.conn.lock().unwrap().inside_io(inside_io);
     }
 }
 
@@ -689,8 +692,6 @@ pub async fn connect<
     let (connected_tx, connected_rx) = oneshot::channel();
 
     let event_handler = server_config.event_handler.take();
-    let event_inside_io: InsideIOSendCallbackArg<ConnectionState<ExtAppState>> =
-        inside_io.clone().into_io_send_callback();
     join_set.spawn(handle_events(
         event_stream,
         keepalive.clone(),
@@ -700,7 +701,6 @@ pub async fn connect<
             .as_ref()
             .is_some_and(|x| x.enable_encoding_at_connect),
         event_handler,
-        event_inside_io,
         connected_tx,
     ));
 
@@ -748,12 +748,13 @@ pub async fn connect<
 
     let (stop_tx, stop_rx) = oneshot::channel();
 
+    let stop_conn = conn.clone();
     let task = tokio::spawn(async move {
         let _join_set = join_set;
         let result = tokio::select! {
             _ = stop_rx => {
                 info!("client shutting down ..");
-                let _ = conn.lock().unwrap().disconnect();
+                let _ = stop_conn.lock().unwrap().disconnect();
                 Ok(ClientResult::UserDisconnect)
             },
             Some(_) = keepalive_task => Err(anyhow!("Keepalive timeout")),
@@ -785,6 +786,7 @@ pub async fn connect<
 
     Ok(ClientConnection {
         task,
+        conn,
         inside_io,
         outside_io,
         connected_signal: Some(connected_rx),
@@ -951,6 +953,8 @@ pub async fn client<
             tracing::error!("Failed to send stop signal");
         }
     });
+
+    connection.set_connection_inside_io();
 
     #[cfg(any(target_os = "linux", target_os = "macos",))]
     connection
