f6404e057d7172de67e293ec8ba50782706fefba
Mariappan Ramasamy <142216110+kp-mariappan-ramasamy@users.noreply.github.com>
2025-08-26 06:28:38 +0800

    app-utils: add windows support for IP_MTU_DISCOVER sockopt module
    - Add conditional compilation for sockopt usage on non-Windows
    - Add Windows specific sockopt functions
    - Disable IP_PKTINFO socket option for windows since it is not needed
      for client

--- lightway-app-utils/src/sockopt/ip_mtu_discover.rs
+++ lightway-app-utils/src/sockopt/ip_mtu_discover.rs
@@ -4,9 +4,20 @@
 //! <https://github.com/rust-lang/socket2/issues/487> we have to reach
 //! for libc and unsafety.
 
+#[cfg(unix)]
 use libc::{IP_PMTUDISC_DO, IP_PMTUDISC_DONT, IP_PMTUDISC_PROBE, IP_PMTUDISC_WANT};
 use std::mem::MaybeUninit;
+#[cfg(unix)]
 use std::os::fd::AsRawFd;
+
+#[cfg(windows)]
+use std::os::windows::io::AsRawSocket;
+
+#[cfg(windows)]
+use windows_sys::Win32::Networking::WinSock::{
+    IPPROTO_IP, IP_MTU_DISCOVER, IP_PMTUDISC_DO, IP_PMTUDISC_DONT, IP_PMTUDISC_NOT_SET,
+    IP_PMTUDISC_PROBE,
+};
 
 /// Enum to represent PMTUd values
 #[derive(Copy, Clone)]
@@ -14,20 +25,27 @@ pub enum IpPmtudisc {
     /// Never send DF frames
     Dont,
     /// Use per route hints
+    #[cfg(unix)]
     Want,
     /// Always DF
     Do,
     /// Ignore dst pmtu
     Probe,
+    /// No explicit setting
+    #[cfg(windows)]
+    NotSet,
 }
 
 impl From<IpPmtudisc> for libc::c_int {
     fn from(value: IpPmtudisc) -> Self {
         match value {
             IpPmtudisc::Dont => IP_PMTUDISC_DONT,
+            #[cfg(unix)]
             IpPmtudisc::Want => IP_PMTUDISC_WANT,
             IpPmtudisc::Do => IP_PMTUDISC_DO,
             IpPmtudisc::Probe => IP_PMTUDISC_PROBE,
+            #[cfg(windows)]
+            IpPmtudisc::NotSet => IP_PMTUDISC_NOT_SET,
         }
     }
 }
@@ -38,9 +56,12 @@ impl TryFrom<libc::c_int> for IpPmtudisc {
     fn try_from(value: libc::c_int) -> Result<Self, Self::Error> {
         match value {
             IP_PMTUDISC_DONT => Ok(IpPmtudisc::Dont),
+            #[cfg(unix)]
             IP_PMTUDISC_WANT => Ok(IpPmtudisc::Want),
             IP_PMTUDISC_DO => Ok(IpPmtudisc::Do),
             IP_PMTUDISC_PROBE => Ok(IpPmtudisc::Probe),
+            #[cfg(windows)]
+            IP_PMTUDISC_NOT_SET => Ok(IpPmtudisc::NotSet),
             v => Err(std::io::Error::other(format!(
                 "unexpected value for IP_PMTUDISC: {:?}",
                 v
@@ -50,6 +71,7 @@ impl TryFrom<libc::c_int> for IpPmtudisc {
 }
 
 /// Get IP_MTU_DISCOVER sockopt
+#[cfg(unix)]
 pub fn get_ip_mtu_discover(sock: &impl AsRawFd) -> std::io::Result<IpPmtudisc> {
     let mut value: MaybeUninit<libc::c_int> = MaybeUninit::uninit();
     let mut len = std::mem::size_of::<libc::c_int>() as libc::socklen_t;
@@ -95,7 +117,43 @@ pub fn get_ip_mtu_discover(sock: &impl AsRawFd) -> std::io::Result<IpPmtudisc> {
     value.try_into()
 }
 
+/// Get IP_MTU_DISCOVER sockopt (Windows)
+#[cfg(windows)]
+pub fn get_ip_mtu_discover(sock: &impl AsRawSocket) -> std::io::Result<IpPmtudisc> {
+    let mut value: MaybeUninit<libc::c_int> = MaybeUninit::uninit();
+    let mut len = std::mem::size_of::<libc::c_int>() as libc::c_int;
+
+    let level = IPPROTO_IP;
+    let optname = IP_MTU_DISCOVER;
+
+    // SAFETY: `getsockopt` requires a socket and a valid buffer of `c_int` size
+    let res = unsafe {
+        libc::getsockopt(
+            sock.as_raw_socket() as usize,
+            level,
+            optname,
+            value.as_mut_ptr().cast(),
+            &mut len,
+        )
+    };
+
+    if res == -1 {
+        return Err(std::io::Error::last_os_error());
+    }
+    if len as usize != std::mem::size_of::<libc::c_int>() {
+        return Err(std::io::Error::other(
+            "unexpected len for IP_DONTFRAGMENT result",
+        ));
+    }
+
+    // SAFETY: `getsockopt` initialised `value` for us.
+    let value = unsafe { value.assume_init() };
+
+    value.try_into()
+}
+
 /// Set IP_MTU_DISCOVER sockopt
+#[cfg(unix)]
 pub fn set_ip_mtu_discover(sock: &impl AsRawFd, pmtudisc: IpPmtudisc) -> std::io::Result<()> {
     let pmtudisc: libc::c_int = pmtudisc.into();
     let len = std::mem::size_of::<libc::c_int>() as libc::socklen_t;
@@ -122,6 +180,34 @@ pub fn set_ip_mtu_discover(sock: &impl AsRawFd, pmtudisc: IpPmtudisc) -> std::io
             level,
             optname,
             &pmtudisc as *const libc::c_int as *const libc::c_void,
+            len,
+        )
+    };
+
+    if res == -1 {
+        Err(std::io::Error::last_os_error())
+    } else {
+        Ok(())
+    }
+}
+
+/// Set IP_MTU_DISCOVER sockopt (Windows)
+#[cfg(windows)]
+pub fn set_ip_mtu_discover(sock: &impl AsRawSocket, pmtudisc: IpPmtudisc) -> std::io::Result<()> {
+    let pmtudisc: libc::c_int = pmtudisc.into();
+
+    let len = std::mem::size_of::<libc::c_int>() as libc::c_int;
+
+    let level = IPPROTO_IP;
+    let optname = IP_MTU_DISCOVER;
+
+    // SAFETY: `setsockopt` requires a socket and a valid buffer of `c_int` size
+    let res = unsafe {
+        libc::setsockopt(
+            sock.as_raw_socket() as usize,
+            level,
+            optname,
+            &pmtudisc as *const libc::c_int as *const libc::c_char,
             len,
         )
     };
